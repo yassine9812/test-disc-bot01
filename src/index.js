8ee3f11a39b5c4a57c0b33ae8657de847f6bb5d5
unknown <yassine50.zayet@gmail.com>
2024-09-19 12:03:17 +0100

    basic push

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+//auto reply
+client.on("ready", (c) => {
+  console.log(`🗿 ${c.user.tag} is online`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "hello") {
+    message.reply("hey!");
+  }
+});
+
+
+//interaction with sclash commands
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number").value;
+    const num2 = interaction.options.get("second-number").value;
+
+    interaction.reply(`the sum is ${num1 + num2}`);
+  }
+});
+
+client.login(process.env.TOKEN);
---
+++ src/register-command.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+const {
+  REST,
+  ROUTES,
+  ApplicationCommandOptionType,
+  ApplicationCommand,
+} = require("discord.js");
+const { Routes } = require("discord-api-types/v10");
+
+const commands = [
+  {
+    name: "add",
+    description: "adds two numbers",
+    options: [
+      {
+        name: "first-number",
+        description: "the first number",
+        type: ApplicationCommandOptionType.Number,
+        choices: [
+          {
+            name: "one",
+            value: 1,
+          },
+          {
+            name: "two",
+            value: 2,
+          },
+          {
+            name: "three",
+            value: 3,
+          },
+        ],
+        required: true,
+      },
+      {
+        name: "second-number",
+        description: "the first number",
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+    ],
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Slash commands were registered successfully!");
+  } catch (error) {
+    console.log(`there was an error: ${error}`);
+  }
+})();
